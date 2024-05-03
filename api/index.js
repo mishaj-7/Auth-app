f60585eb0d62a35612f26740120f3ed27fe943a8
Mishaj__k <amstig100@gmail.com>
2024-05-03 15:09:19 +0530

    created user model

--- api/index.js
+++ api/index.js
@@ -6,11 +6,11 @@ dotenv.config();
 mongoose.connect(process.env.MONGO).then(() => {
      console.log('databse connected...')
 }).catch((err) => {
-     console.log(err);
+    console.log(err);
  })
 
 const app = express();
 
 app.listen(3001, () => {
-    console.log('server listening on port 3000');
+    console.log('server listening on port 3001');
 });
