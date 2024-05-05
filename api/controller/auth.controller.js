3eac1f55d6387d11144fa0a9efefd7b773516a14
Mishaj__k <amstig100@gmail.com>
2024-05-05 20:59:44 +0530

    singin api create

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
-// import { erroHandler } from "../utils/error.js";
+import { erroHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -15,3 +16,22 @@ export const signup = async (req, res, next) => {
     }
     
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(erroHandler(404, 'user not found'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(erroHandler(401, 'wrong credential'));
+         
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        res.cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(validUser)
+    } catch (err) {
+        console.log("helo")
+        next(err)
+     }
+}
